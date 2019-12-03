b16a25f2f0c5b16be60d53b9b43e75452caffb72
Nathan Broadbent <git@ndbroadbent.com>
2019-12-04 02:48:07 +0700

    Update example with single quotes

--- example_code/example.rb
+++ example_code/example.rb
@@ -1,7 +1,8 @@
 def fuel_for(mass)
+  puts "Calculating fuel for mass: #{mass}"
   fuel = (mass.to_i / 3).floor.to_i - 2
   return 0 if fuel <= 0
   fuel + fuel_for(fuel)
 end
 
-puts File.read(File.join(__DIR__, "input")).lines.sum { |mass| fuel_for(mass) }
+puts File.read(File.join(__DIR__, 'input')).lines.sum { |mass| fuel_for(mass) }
