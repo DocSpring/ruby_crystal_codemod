ce4f9d60d2e2f69edf649d901116be824d0522e8
Nathan Broadbent <git@ndbroadbent.com>
2019-12-04 02:38:27 +0700

    Added an example from adventofcode.com day 1

---
+++ example_code/example.rb
@@ -0,0 +1,7 @@
+def fuel_for(mass)
+  fuel = (mass.to_i / 3).floor.to_i - 2
+  return 0 if fuel <= 0
+  fuel + fuel_for(fuel)
+end
+
+puts File.read(File.join(__DIR__, "input")).lines.sum { |mass| fuel_for(mass) }
