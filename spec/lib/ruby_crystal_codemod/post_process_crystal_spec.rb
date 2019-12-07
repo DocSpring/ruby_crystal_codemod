e17bf812998cfb1540fb94681bbdc6ecd49f86e5
Nathan Broadbent <git@ndbroadbent.com>
2019-12-07 14:14:14 +0700

    Formatted spec with rufo. it's a bit broken and weird, but at least it's idempotent

--- spec/lib/ruby_crystal_codemod/post_process_crystal_spec.rb
+++ spec/lib/ruby_crystal_codemod/post_process_crystal_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PostProcessCrystal do
   it "mocks the call to File.read_lines" do
     allow(PostProcessCrystal).to receive(:file_read_lines).with("example_file").and_return([
       "hey, world!",
-      "how's it hanging"
+      "how's it hanging",
     ])
 
     ppc = PostProcessCrystal.new("example_file")
@@ -13,8 +13,10 @@ RSpec.describe PostProcessCrystal do
   end
 
   it "removes any content between #~# BEGIN ruby and #~# END ruby" do
-    allow(PostProcessCrystal).to receive(:file_read_lines).with("example_file").and_return(
-      <<-CODE.lines.map(&:chomp)
+    allow(PostProcessCrystal).to receive(:file_read_lines)
+                                   .with("example_file")
+                                   .and_return(
+                                     <<-CODE.lines.map(&:chomp)
       def foo
         #~# BEGIN ruby
         return 234
@@ -28,24 +30,28 @@ RSpec.describe PostProcessCrystal do
 
       puts foo
       CODE
-    )
+
+                                   
+)
 
     ppc = PostProcessCrystal.new("example_file")
     ppc.post_process_crystal
     expect(ppc.contents).to eq(
-      <<-CODE
+                              <<-CODE
       def foo
         123
       end
 
       puts foo
       CODE
-    )
+
+                            
+)
   end
 
   it "uncomments any content between #~# BEGIN crystal and #~# END crystal" do
     allow(PostProcessCrystal).to receive(:file_read_lines).with("example_file").and_return(
-      <<-CODE.lines.map(&:chomp)
+                                   <<-CODE.lines.map(&:chomp)
       def foo
         123
         #~# BEGIN crystal
@@ -58,12 +64,14 @@ RSpec.describe PostProcessCrystal do
       #~# END crystal
       puts foo
       CODE
-    )
+
+                                 
+)
 
     ppc = PostProcessCrystal.new("example_file")
     ppc.post_process_crystal
     expect(ppc.contents).to eq(
-      <<-CODE
+                              <<-CODE
       def foo
         123
         return 456
@@ -72,12 +80,14 @@ RSpec.describe PostProcessCrystal do
       # comment should be present
       puts foo
       CODE
-    )
+
+                            
+)
   end
 
   it "removes all Ruby lines and uncomments all Crystal lines" do
     allow(PostProcessCrystal).to receive(:file_read_lines).with("example_file").and_return(
-      <<-CODE.lines.map(&:chomp)
+                                   <<-CODE.lines.map(&:chomp)
       class Foo
         property :foo, :bar
 
@@ -95,14 +105,16 @@ RSpec.describe PostProcessCrystal do
         #~# END crystal
       end
       CODE
-    )
+
+                                 
+)
 
     ppc = PostProcessCrystal.new("example_file")
     ppc.post_process_crystal
     # NOTE: This is a post-processing step. The rufo formatting code
     # must run before this, and will translate attr_accessor into property, etc.
     expect(ppc.contents).to eq(
-      <<-CODE
+                              <<-CODE
       class Foo
         property :foo, :bar
 
@@ -112,6 +124,8 @@ RSpec.describe PostProcessCrystal do
         end
       end
       CODE
-    )
+
+                            
+)
   end
 end
